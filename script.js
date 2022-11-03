9865b166b03a80921f44b79d8755c99c82baf381
samuelindberg <samuel.lindberg01@icloud.com>
2022-11-03 10:46:45 +0100

    login fungerar med array

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ let user = ["lossallos","Testpass"];
 function loginFunction(){
     let userName = document.getElementById("username").value;
     let password = document.getElementById("password").value;
-    if(userName=user[0] && password=user[1]){
+    if(userName==user[0] && password==user[1]){
         alert("rätt uppgifter");
     }   
+    else{
+        alert("fel uppgifter");
+    }
 }
