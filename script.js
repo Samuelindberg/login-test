25c9cc4e27364f916b6bd5f42e503e3d3c59ada9
samuelindberg <samuel.lindberg01@icloud.com>
2022-11-03 10:15:12 +0100

    input och function v.1

---
+++ script.js
@@ -0,0 +1,8 @@
+let user = ["lossallos","Testpass"];
+function loginFunction(){
+    let userName = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    if(userName=user[0] && password=user[1]){
+        alert("rätt uppgifter");
+    }   
+}
