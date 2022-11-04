7a119d8061c4f94eadae89b6be6718925f39a0b3
samuelindberg <samuel.lindberg01@icloud.com>
2022-11-04 15:26:54 +0100

    loggin funkar och h1-tagg visas

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 const succesful = document.getElementById("conformation");
-let user ={
-    users:["lossallos","salle1","salle2","salle3","elsaF"],
-    password: ["Testpass","salle1","salle2","salle3","elsa!"]
+const form = document.querySelector("form");
+let user = {
+  users: ["lossallos", "salle1", "salle2", "salle3", "elsaF"],
+  password: ["Testpass", "salle1", "salle2", "salle3", "elsa!"],
 };
 let rightUser = false;
-function loginFunction(){
-    const person = {
-         usernameInput: document.getElementById("username").value,
-         passwordInput: document.getElementById("password").value
-    };
-        for(i=0;i<user.users.length;i++){
-          
-            if(person.usernameInput == user.users[i] && person.passwordInput == user.password[i]){
-                succesful.style.color="black"; 
-             rightUser=true; 
-             i=user.users.length;
-            }
-        }
+function loginFunction() {
+  const person = {
+    usernameInput: document.getElementById("username").value,
+    passwordInput: document.getElementById("password").value,
+  };
+  for (i = 0; i < user.users.length; i++) {
+    if (
+      person.usernameInput == user.users[i] &&
+      person.passwordInput == user.password[i]
+    ) {
+      rightUser = true;
+      i = user.users.length;
     }
+  }
+}
+form.addEventListener("submit",function(event){
+    event.preventDefault()
+    loginFunction()
+    if (rightUser) {
+        succesful.style.color = "black";
+      } else{
+        succesful.textContent ="wrong password";
+        succesful.style.color = "red";
+    }
+    //   reset form
+      document.getElementById("username").value="";
+      document.getElementById("password").value="";
+})
