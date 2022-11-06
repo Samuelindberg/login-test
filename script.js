c1c2b38530674beac26a5ba7b1620691254ce020
samuelindberg <samuel.lindberg01@icloud.com>
2022-11-06 12:21:54 +0100

    skapa konto layout och boilerplate

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+//Login script
 const succesful = document.getElementById("conformation");
+const accountCreated = document.getElementById("create-complete");
 const form = document.querySelector("form");
+let rightUser = false;
+let samePassword = false;
+let userExist = true;
 let user = {
   users: ["lossallos", "salle1", "salle2", "salle3", "elsaF"],
   password: ["Testpass", "salle1", "salle2", "salle3", "elsa!"],
 };
-let rightUser = false;
+
+
 function loginFunction() {
   const person = {
     usernameInput: document.getElementById("username").value,
-    passwordInput: document.getElementById("password").value,
+    passwordInput: document.getElementById("password").value
   };
   for (i = 0; i < user.users.length; i++) {
     if (
@@ -33,3 +39,19 @@ form.addEventListener("submit",function(event){
       document.getElementById("username").value="";
       document.getElementById("password").value="";
 })
+//create account script
+  function createFunction(){
+    const newUsername = {
+        username: document.getElementById("newUsername").value,
+        password: document.getElementById("newPassword").value,
+        confirmPassword: document.getElementById("newPasswordConformation").value
+    };
+    user.users.push(newUsername.username);
+    alert(user.users[5]);
+    form.addEventListener("submit",function(event){
+        event.preventDefault()
+        createFunction()
+        //   reset form
+          document.getElementById("username").value="";
+          document.getElementById("password").value="";
+  })
